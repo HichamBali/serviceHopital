e532fdacc4d4c50861d4d07c72276074dc1b7ca7
hicham bali <balihicham01@gmail.com>
2017-11-06 14:04:00 +0100

    ajouter medecin fonctionnel

--- ajouterMedecin.php
+++ ajouterMedecin.php
@@ -6,26 +6,20 @@
  * Time: 17:12
  */
 
-$nom=$_POST['nom'];
-$prenom=$_POST['prenom'];
-$adresse=$_POST['adresse'];
-$grade=$_POST['grade'];
-$specialite=$_POST['specialite'];
-$numTel=$_POST['telephone'];
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+$adresse = $_POST['adresse'];
+$grade = $_POST['grade'];
+$specialite = $_POST['specialite'];
+$numTel = $_POST['telephone'];
+echo $nom;
 try {
     //connexion à la base de donnée
-    $connexionDB = new PDO("mysql:host=localhost;dbname=service", "root", "");
-    $connexionDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connexionDB = new PDO('mysql:host=localhost;dbname=service', 'root', '');
     $insert = $connexionDB->query("INSERT INTO medecin(nom_m,prenom_m,adresse_m,grade_m,specialite_m,numTel_m)
-      VALUES (:nom_medecin,:prenom_medecin,:adresse_medecin,:grade_medecin,:specialite_medecin,:numTel_medecin)");
-    $insert->bindParam(':nom_medecin',$nom);
-    $insert->bindParam(':prenom_medecin',$prenom);
-    $insert->bindParam(':adresse_medecin',$adresse);
-    $insert->bindParam(':agrade_medecin',$grade);
-    $insert->bindParam(':specialite_medecin',$specialite);
-    $insert->bindParam(':numTel_medecin',$numTel);
+      VALUES ('".$nom."','".$prenom."','".$adresse."','".$grade."','".$specialite."','".$numTel."')");
     $insert->execute();
-    echo "insertion reussite";
+    header("location:afficherMedecin.php");
 } catch
 (PDOException $e) {
     die("Erreur: " . $e->getMessage());
